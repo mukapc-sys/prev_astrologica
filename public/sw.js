2ebe36ba2900ccda88994ce35be2de59506d3e36
mukapc-sys <mukapc@gmail.com>
2026-07-21 23:09:59 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "pac-v2";
-const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "pac-v3";
+const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg", "/cidades.json"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x))))); self.clients.claim(); });
 self.addEventListener("fetch", e => {
